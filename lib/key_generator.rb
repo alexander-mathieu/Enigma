d24e5ecf8163accd6b4e502b5c4b6e64dddb3498
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-04-12 10:34:27 -0600

    Add key method and tests

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -1,3 +1,13 @@
+require 'securerandom'
+
 class KeyGenerator
+
+  def initialize
+    @key = key
+  end
+
+  def key
+    5.times.map {SecureRandom.random_number(9)}.join.to_i
+  end
 
 end
--- test/key_generator_test.rb
+++ test/key_generator_test.rb
@@ -11,4 +11,12 @@ class KeyGeneratorTest < MiniTest::Test
     assert_instance_of KeyGenerator, @key_generator
   end
 
+  def test_it_has_a_random_key
+    assert_includes 0..99999, @key_generator.key
+  end
+
+  def test_the_key_is_5_digits
+    assert_equal 5, @key_generator.key.digits.length
+  end
+
 end
