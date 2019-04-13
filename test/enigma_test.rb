0465584b338a657fd2a6e2ab45d2c914f3cf2ff9
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-04-13 13:41:02 -0600

    Add skeleton of Enigma class and test file

---
+++ test/enigma_test.rb
@@ -0,0 +1,13 @@
+require './test/test_helper'
+require './lib/key_generator'
+require './lib/offset_generator'
+require './lib/caesar_cipher'
+require './lib/enigma'
+
+class EnigmaTest < MiniTest::Test
+
+  def setup
+
+  end
+
+end
