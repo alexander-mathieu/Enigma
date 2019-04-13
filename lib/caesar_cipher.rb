a01041a7b624351230a5aeda3e97a7f4939467cc
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-04-13 13:10:02 -0600

    Add initialize method and attribute tests

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -1,3 +1,14 @@
 class CaesarCipher
+  attr_reader :letter,
+              :shift_value,
+              :alphabet
+
+  def initialize(letter, shift_value)
+    @letter      = letter
+    @shift_value = shift_value.to_i
+    @alphabet    = ('a'..'z').to_a << " "
+  end
+
+  
 
 end
--- test/caesar_cipher_test.rb
+++ test/caesar_cipher_test.rb
@@ -4,11 +4,25 @@ require './lib/caesar_cipher'
 class CaesarCipherTest < MiniTest::Test
 
   def setup
-    @caesar_cipher = CaesarCipher.new
+    @caesar_cipher = CaesarCipher.new("a", "73")
   end
 
   def test_it_exists
     assert_instance_of CaesarCipher, @caesar_cipher
+  end
+
+  def test_it_has_a_letter
+    assert_equal "a", @caesar_cipher.letter
+  end
+
+  def test_it_has_a_shift_value
+    assert_equal 73, @caesar_cipher.shift_value
+  end
+
+  def test_it_has_an_alphabet
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+
+    assert_equal expected, @caesar_cipher.alphabet
   end
 
 end
