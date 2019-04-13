9fed5f6d1caa9c03c5bae025cf71675d1d91fc53
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-04-13 14:43:45 -0600

    Add assertion to date attribute test

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -20,12 +20,14 @@ class EnigmaTest < MiniTest::Test
 
   def test_by_default_it_has_a_5_digit_key
     assert_instance_of String, @enigma.key
-    
+
     assert_equal 5, @enigma.key.length
   end
 
-  def test_by_default_it_has_a_date
-    assert_equal "041319", @enigma.date
+  def test_by_default_it_has_a_6_digit_date
+    assert_instance_of String, @enigma.date
+
+    assert_equal 6, @enigma.date.length
   end
 
 end
