83211dbbc75b919b8c09b29ecf43de2f9793b17e
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-04-13 15:31:40 -0600

    Add offset_assignment method

--- lib/assignable.rb
+++ lib/assignable.rb
@@ -1,3 +1,17 @@
 module Assignable
 
+  def key_assignment(key)
+    {"A" => key[0..1].join,
+     "B" => key[1..2].join,
+     "C" => key[2..3].join,
+     "D" => key[3..4].join}
+  end
+
+  def offset_assignment(offset)
+    {"A" => offset[0],
+     "B" => offset[1],
+     "C" => offset[2],
+     "D" => offset[3]}
+  end
+
 end
--- test/assignable_test.rb
+++
@@ -1,6 +0,0 @@
-require './test/test_helper'
-require './lib/assignable'
-
-class AssignableTest < MiniTest::Test
-
-end
