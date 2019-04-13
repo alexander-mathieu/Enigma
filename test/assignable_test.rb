413d7b61f46b2b0623b1a964b377d4db2cc79c6d
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-04-13 15:24:00 -0600

    Add skeleton of Assignable module and tests

---
+++ test/assignable_test.rb
@@ -0,0 +1,6 @@
+require './test/test_helper'
+require './lib/assignable'
+
+class AssignableTest < MiniTest::Test
+
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,9 +4,9 @@ SimpleCov.start
 
 require 'minitest/autorun'
 require 'minitest/pride'
-require 'mocha/minitest'
 
 require './lib/key_generator'
 require './lib/offset_generator'
 require './lib/caesar_cipher'
+require './lib/assignable'
 require './lib/enigma'
