4b565ecc60ba2f27b874e7c6d0356dcc75fbe2c0
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-04-13 15:44:01 -0600

    Remove key_array method and optional argument from KeyGenerator, update tests

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -3,24 +3,12 @@ require 'securerandom'
 class KeyGenerator
   attr_reader :key
 
-  def initialize(key = default)
-    @key            = key
-    @key_assignment = key_assignment
+  def initialize
+    @key = key
   end
 
-  def default
+  def key
     5.times.map {SecureRandom.random_number(9).to_s}.join
-  end
-
-  def key_array
-    @key.split("")
-  end
-
-  def key_assignment
-    {"A" => key_array[0..1].join,
-     "B" => key_array[1..2].join,
-     "C" => key_array[2..3].join,
-     "D" => key_array[3..4].join}
   end
 
 end
--- test/key_generator_test.rb
+++ test/key_generator_test.rb
@@ -12,18 +12,9 @@ class KeyGeneratorTest < MiniTest::Test
   end
 
   def test_by_default_it_generates_an_array_of_5_numbers
+    assert_instance_of String, @key_generator.key
+
     assert_equal 5, @key_generator.key.length
   end
-
-   def test_it_can_join_array_into_a_key_with_5_digits
-     assert_equal 5, @key_generator.key_array.length
-   end
-
-  def test_it_can_split_key_into_a_hash_with_4_sets_of_2_digits
-    assert_equal 2, @key_generator.key_assignment["A"].length
-    assert_equal 2, @key_generator.key_assignment["B"].length
-    assert_equal 2, @key_generator.key_assignment["C"].length
-    assert_equal 2, @key_generator.key_assignment["D"].length
-  end
-
+  
 end
