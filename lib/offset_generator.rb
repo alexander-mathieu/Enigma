2f703fd1547e4a6f8f8ffa95f5663d7745a61867
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-04-13 15:41:46 -0600

    Remove optional argument from OffsetGenerator, change offset_array method to offset, now provides a single string instead of an array

--- lib/offset_generator.rb
+++ lib/offset_generator.rb
@@ -3,29 +3,29 @@ require 'date'
 class OffsetGenerator
   attr_reader :date
 
-  def initialize(date = default)
+  def initialize
     @date = date
   end
 
-  def default
-    default = Date.today
-    default.strftime("%m%d%y")
+  def date
+    date = Date.today
+    date.strftime("%m%d%y")
   end
 
-  def offset_array
-    stringify.split("").last(4)
-  end
-
-  def stringify
-    square_date.to_s
+  def date_to_integer
+    @date.to_i
   end
 
   def square_date
     date_to_integer ** 2
   end
 
-  def date_to_integer
-    @date.to_i
+  def stringify
+    square_date.to_s
+  end
+
+  def offset
+    stringify.split("").last(4).join
   end
 
 end
