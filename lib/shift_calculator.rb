0e15ae95345d88cb84752c98419a16769b1cdaee
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-04-13 16:58:14 -0600

    Change Assignable module to ShiftCalculator class and create test file

---
+++ lib/shift_calculator.rb
@@ -0,0 +1,7 @@
+class ShiftCalculator
+
+  def initialize(argument)
+    @argument = argument
+  end
+
+end
---
+++ test/shift_calculator_test.rb
@@ -0,0 +1,6 @@
+require './test/test_helper'
+require './lib/shift_calculator'
+
+class ShiftCalculatorTest < MiniTest::Test
+
+end
