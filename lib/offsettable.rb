65909c95e30f964272ab776ab8cae6a706ae9a5e
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-04-14 15:57:27 -0600

    Denote private methods within Offsettable

--- lib/offsettable.rb
+++ lib/offsettable.rb
@@ -1,19 +1,21 @@
 module Offsettable
 
-  def date_to_integer
-    date.to_i
+  def calculate_offset(date)
+    stringify.split("").last(4).join
+  end
+
+private
+
+  def stringify
+    square_date.to_s
   end
 
   def square_date
     date_to_integer ** 2
   end
 
-  def stringify
-    square_date.to_s
-  end
-
-  def calculate_offset(date)
-    stringify.split("").last(4).join
+  def date_to_integer
+    date.to_i
   end
 
 end
